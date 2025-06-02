a1f87e1aecdf1268a85885844b53efbeeb6d3758
artego974 <artego974@gmail.com>
2025-06-02 15:11:50 -0300

    header responsivo

---
+++ public/js/script.js
@@ -0,0 +1,13 @@
+const openBtn = document.getElementById('menuButton');
+  const sideMenu = document.getElementById('menuOverlay');
+
+  openBtn.addEventListener('click', function (e) {
+    e.stopPropagation();
+    sideMenu.classList.toggle('show');
+  });
+
+  document.addEventListener('click', function (e) {
+    if (!sideMenu.contains(e.target) && !openBtn.contains(e.target)) {
+      sideMenu.classList.remove('show');
+    }
+  });
