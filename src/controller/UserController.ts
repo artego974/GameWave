5a644c6a658d1dd1c854d4ddf639b8f5057e023f
paulo <paulo200825@gmail.com>
2025-06-05 17:08:17 -0300

    back

---
+++ src/controller/UserController.ts
@@ -0,0 +1,92 @@
+import {Request, Response} from "express"
+import { User } from "../model/User"
+import { AppDataSource } from "../config/data-source"
+
+const userRepository = AppDataSource.getRepository(User);
+
+export class ProductController{
+    async list(req: Request,res:Response){
+        const prod = await userRepository.find();
+        res.json(prod);
+        return;
+    }
+
+    async create(req:Request,res:Response){
+        const {name,email,nickName,password} = req.body;
+        if(name == '' || nickName =='' || password == ''|| email == ''){
+            res.status(400).json({  messagem: "Preencha todos os campos!" })
+            return
+        }else{
+            const user = userRepository.create({name,email,nickName,password});
+            await userRepository.save(user);
+            res.status(201).json(user);
+            return;
+        }  
+    }
+
+    async delete(req:Request,res:Response){
+        const {id} = req.body;
+
+        const user = await userRepository.findOneBy({id:Number(id)});
+
+        if(!user){
+            res.status(401).json({ menssagem: "User não encontrado" });
+            return;
+        }
+        await userRepository.remove(user);
+        res.status(204).send();
+        return;
+    }
+
+    async show(req:Request,res:Response){
+        const {id} = req.params;
+        const user = await userRepository.findOneBy({id:Number(id)});
+        if(!user){
+            res.status(404).json({menssagem: "User não encontrado!"});
+            return;
+        }
+        res.status(201).json(user);
+        return;
+    }
+
+    async shew(req:Request,res:Response){
+        const {name} = req.params;
+        const user = await userRepository.findOneBy({name:String(name)});
+
+        if(!user){
+            res.status(404).json({menssagem: "User não encontrado!"});
+            return;
+        }
+        res.status(201).json(user);
+        return;
+    }
+
+    async update(req:Request,res:Response){
+        const {id} = req.params;
+        const {name,nickName,email,password} = req.body;
+
+        const prod = await userRepository.findOneBy({id:Number(id)});
+
+        if(!prod){
+            res.status(404).json({menssagem: "Produto não encontrado"})
+            return;
+        }
+
+        if(name != ''){
+            prod.name = name;
+        }
+        if(nickName != ''){
+            prod.nickName = nickName;
+        }
+        if(email != ''){
+            prod.email = email;
+        }
+        if(password != ''){
+            prod.password = password;
+        }
+
+        await userRepository.save(prod)
+        res.json(prod);
+        return;
+    }
+}
---
+++ src/model/User.ts
@@ -0,0 +1,99 @@
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from "typeorm";
+
+@Entity('users')
+export class User {
+
+    @PrimaryGeneratedColumn()
+    id!: number;
+
+    @Column({type:'varchar', length: 100, nullable: false })
+    private _name: string;
+
+    @Column({ unique: true })
+    private _email: string;
+
+    @Column({type:'varchar', nullable: false })
+    private _password: string;
+
+    @Column({type:'varchar', length: 100, nullable: false, unique: true})
+    private _nickName: string;
+
+    @Column({type: "decimal"})
+    seguidores!: number
+
+    @Column({       })
+    seguindo!: number
+
+
+    constructor(name:string,email:string,password:string,nickName:string){
+        this._nickName = nickName
+        this._name = name;
+        this._email = email;
+        this._password = password;
+    }
+
+    /**
+     * Getter name
+     * @return {string}
+     */
+	public get name(): string {
+		return this._name;
+	}
+
+    /**
+     * Getter email
+     * @return {string}
+     */
+	public get email(): string {
+		return this._email;
+	}
+
+    /**
+     * Getter password
+     * @return {string}
+     */
+	public get password(): string {
+		return this._password;
+	}
+
+    /**
+     * Getter nickName
+     * @return {string}
+     */
+	public get nickName(): string {
+		return this._nickName;
+	}
+
+    /**
+     * Setter name
+     * @param {string} value
+     */
+	public set name(value: string) {
+		this._name = value;
+	}
+
+    /**
+     * Setter email
+     * @param {string} value
+     */
+	public set email(value: string) {
+		this._email = value;
+	}
+
+    /**
+     * Setter password
+     * @param {string} value
+     */
+	public set password(value: string) {
+		this._password = value;
+	}
+
+    /**
+     * Setter nickName
+     * @param {string} value
+     */
+	public set nickName(value: string) {
+		this._nickName = value;
+	}
+
+}
