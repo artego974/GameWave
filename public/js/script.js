089f226b8f26d845078a6ff3296959591947c746
artego974 <artego974@gmail.com>
2025-06-18 16:13:10 -0300

    front com alterações

--- public/js/script.js
+++ public/js/script.js
@@ -5,15 +5,21 @@ document.addEventListener('DOMContentLoaded', function () {
   openBtn.addEventListener('click', function (e) {
     e.stopPropagation();
     sideMenu.classList.toggle('show');
+    sideMenu.classList.toggle('hide');
   });
 
   document.addEventListener('click', function (e) {
     if (!sideMenu.contains(e.target) && !openBtn.contains(e.target)) {
       sideMenu.classList.remove('show');
+      sideMenu.classList.add('hide');
+      openBtn.style.display = 'block';
     }
   });
 
   sideMenu.addEventListener('click', function (e) {
     e.stopPropagation();
+    sideMenu.classList.remove('show');
+    sideMenu.classList.add('hide');
+    openBtn.style.display = 'block';
   });
 });
