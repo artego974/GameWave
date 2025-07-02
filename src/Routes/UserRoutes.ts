fa72fdcc475822a8d3511ff552eb40f102a8622d
artego974 <artego974@gmail.com>
2025-07-02 16:33:16 -0300

    arrumando os bagui

--- src/Routes/UserRoutes.ts
+++ src/Routes/UserRoutes.ts
@@ -1,13 +1,13 @@
 import { Router } from "express";
-import { UserController } from "../controller/UserController";
+import { UserController } from "../controller/UserController"
 
 const router = Router();
 const userController = new UserController();
 
-router.post("/User", userController.create);
-router.get("/User", userController.list);
-router.get("/User/:id", userController.show);
-router.put("/User/:id", userController.update);
-router.delete("/User/:id", userController.delete);
+router.post("/user", userController.create);
+router.get("/user", userController.list);
+router.get("/user/:id", userController.show);
+router.put("/user/:id", userController.update);
+router.delete("/user/:id", userController.delete);
 
 export default router;
